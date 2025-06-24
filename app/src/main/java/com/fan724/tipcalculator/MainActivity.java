63b065834093390166992467669ff4981290d9e9
fan724 <fanil724@gmail.com>
2025-06-24 18:54:07 +0300

    init

---
+++ app/src/main/java/com/fan724/tipcalculator/MainActivity.java
@@ -0,0 +1,69 @@
+package com.fan724.tipcalculator;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        SeekBar seekBar = findViewById(R.id.seekBar);
+        EditText editSum = findViewById(R.id.inputSum);
+        TextView textTip = findViewById(R.id.tip);
+        TextView endZak = findViewById(R.id.endZak);
+        TextView endTip = findViewById(R.id.endTip);
+        TextView endSum = findViewById(R.id.endSum);
+
+        Button btn = findViewById(R.id.btn);
+
+        btn.setOnClickListener(v -> {
+            String text = editSum.getText().toString();
+            endZak.setText(text);
+            Integer s;
+            if (text.isEmpty()) {
+                s = 0;
+            } else {
+                s = (Integer.parseInt(text) * seekBar.getProgress()) / 100;
+            }
+
+            textTip.setText(String.valueOf(s));
+            endTip.setText(String.valueOf(s));
+            endSum.setText(String.valueOf(Integer.parseInt(text) + s));
+
+        });
+
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
+
+                int sumZak = Integer.parseInt(editSum.getText().toString());
+                int sumTip = (sumZak * progress) / 100;
+                textTip.setText(String.valueOf(sumTip));
+                endTip.setText(String.valueOf(sumTip));
+                endSum.setText(String.valueOf(sumZak + sumTip));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                Log.d("SeekBar", "Пользователь начал перемещение");
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                Log.d("SeekBar", "Пользователь закончил перемещение");
+            }
+        });
+    }
+}
